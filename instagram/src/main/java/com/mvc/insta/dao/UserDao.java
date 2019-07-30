9c4d51a4da2e2fa3240fc37db9f24c5f988a69a4
hanahreum2 <han_7654@naver.com>
2019-07-30 12:54:26 +0900

    이거뭐야

--- instagram/src/main/java/com/mvc/insta/dao/UserDao.java
+++ instagram/src/main/java/com/mvc/insta/dao/UserDao.java
@@ -6,13 +6,10 @@ import com.mvc.insta.vo.UserVO;
 
 public interface UserDao {
 	public void insertUser(UserVO userVO) throws Exception;
-<<<<<<< HEAD
 
 	public UserVO loginUser(UserVO userVO) throws Exception;
 
-=======
 	public List<UserVO> userList() throws Exception;
 	public UserVO viewUser(String user_email) throws Exception;
 	
->>>>>>> 9ed9b3f539f9ae8e9373f755aabc1098d035faa8
 }
--- instagram/src/main/java/com/mvc/insta/service/UserService.java
+++ instagram/src/main/java/com/mvc/insta/service/UserService.java
@@ -7,13 +7,9 @@ import com.mvc.insta.vo.UserVO;
 
 public interface UserService {
 	public void insertUser(UserVO userVO)throws Exception;
-<<<<<<< HEAD
-
 	public UserVO loginUser(UserVO userVO)throws Exception;
-=======
 	public List<UserVO> userList() throws Exception;
 	public UserVO viewUser(String user_email) throws Exception;
->>>>>>> 9ed9b3f539f9ae8e9373f755aabc1098d035faa8
 		
 	
 
